283437cbcaf35164e866323b82468a66978735c3
coolguyjuls <129122097+coolguyjuls@users.noreply.github.com>
2025-07-03 02:53:42 -0500

    Rename Variables, Adds New GLobal Variables
    Renamed variables to A and B, and global variables
     to fixed issue arising with dividing by zero
     setting the variable stringA to Infinity, and
     allowing Infinity have operations performed on.

--- index.js
+++ index.js
@@ -16,92 +16,141 @@ const buttonEnter = document.getElementById("buttonEnter")
 
 const calculatorDisplay = document.getElementById("displayText")
 
-
-
-let operation = '';
-let valueStoreX =``;
-let valueStoreY = ``;
-let operatorCheck = false;
-
-// button1.addEventListener("click", ()=> calculatorArray.push(1))
-button9.addEventListener("click", ()=> test(9))
-// button2.addEventListener("click", ()=> calculatorArray.push(2))
-button8.addEventListener("click", ()=> test(8))
-button7.addEventListener("click", ()=> test(7))
-button6.addEventListener("click", ()=> test(6))
-button5.addEventListener("click", ()=> test(5))
-button4.addEventListener("click", ()=> test(4))
-button3.addEventListener("click", ()=> test(3))
-button2.addEventListener("click", ()=> test(2))
-button1.addEventListener("click", ()=> test(1))
-button0.addEventListener("click", ()=> test(0))
-// buttonMultiply.addEventListener("click", ()=> operation = `*`)
+button9.addEventListener("click", ()=> test(`9`))
+button8.addEventListener("click", ()=> test(`8`))
+button7.addEventListener("click", ()=> test(`7`))
+button6.addEventListener("click", ()=> test(`6`))
+button5.addEventListener("click", ()=> test(`5`))
+button4.addEventListener("click", ()=> test(`4`))
+button3.addEventListener("click", ()=> test(`3`))
+button2.addEventListener("click", ()=> test(`2`))
+button1.addEventListener("click", ()=> test(`1`))
+button0.addEventListener("click", ()=> test(`0`))
 buttonMultiply.addEventListener("click", ()=> operatorSet(`*`))
 buttonDivide.addEventListener("click", ()=> operatorSet(`/`))
 buttonAdd.addEventListener("click", ()=> operatorSet(`+`))
 buttonSubtract.addEventListener("click", ()=> operatorSet(`-`))
-// buttonEnter.addEventListener("click", inputOperation)
 buttonEnter.addEventListener("click", executionCheck)
 
 
-function test(num){
+let operation = '';
+let stringA =``;
+let stringB = ``;
+let a;
+let b;
+
+// variable check for switching between setting the value for stringA and string B. False: stringA. True: stringB.
+let operatorCheck = false;
+// Disables button to make sure variables a and b will have values before being allowed to move onto the execution function.
+buttonEnter.disabled = true;
+// if false, numbers entered will concatenate. If true, the first variable is already initialized with the value from result, and a number entered afterwards will clear it out.
+resultEqualToA = false;
+
+
+
+function test(string){
     console.log(`test function here`);
     
     if (operatorCheck === false){
-        if (valueStoreX >0){
-            valueStoreX =0;
+
+        if (resultEqualToA === true){
+            stringA = string;
+            resultEqualToA = false;
+            calculatorDisplay.innerText = stringA;
+            console.log(`stringA check: `, stringA)
+            console.log(string)
+
+        } else if (resultEqualToA != true){
+            stringA += string;
+            calculatorDisplay.innerText = stringA
+            
+            
+            console.log(`stringA check, ${stringA}`);
+            console.log(`operator check 1: ${operatorCheck}`);
+            console.log(`result equal to A check 1: ${resultEqualToA}`);
+            
         }
-        valueStoreX += (num);
-        console.log(`valuestoreX check 1, ${valueStoreX}`);
-        console.log(operatorCheck);
-        calculatorDisplay.innerText = String(valueStoreX)
-        
+    // calculatorDisplay.innerText += string
         
     } else if (operatorCheck === true){
-        valueStoreY += (num);
-        console.log(`valuestoreY check 1, ${valueStoreY}`);
-        calculatorDisplay.innerText += ` `+ String(valueStoreY)
+        stringB += string;
+
+        calculatorDisplay.innerText = `${stringA} ${operator} ${stringB}`;
+        buttonEnter.disabled = false;
+
+
+        console.log(`stringB check, ${stringB}`);
+        console.log(`operator check 1: ${operatorCheck}`);
+        console.log(`result equal to A check 2: ${resultEqualToA}`);
     }
 }
 
 function operatorSet(operation){
     operator = operation;
     operatorCheck = true;
+    calculatorDisplay.innerText = `${stringA} ${operator} `;
+    
+
+
     console.log(`operatorSet function check : ${operator}`);
-    calculatorDisplay.innerText += ` ${operator}`
+    console.log(`result equal to A check 3: ${resultEqualToA}`);
     return operator;
     
 }
 
+function infinityCheck(){
+    if (stringA == Infinity){
+        
+    }
+}
 
 function executionCheck(){
     let result;
-    let x = Number(valueStoreX);
-    let y = Number(valueStoreY);
+    let a = Number(stringA);
+    let b = Number(stringB);
      
     if (operatorCheck === true){
 
         switch (operator) {
-            case `*`: result = x * y ;
+            case `*`: result = a * b ;
             break;
-            case `/`: result = x / y;
+            case `/`: result = a / b;
             break;
-            case `+`: result = x + y;
+            case `+`: result = a + b;
             break;
-            case `-`: result = x - y;
+            case `-`: result = a - b;
             break;
         }
+    } else if(operatorCheck === false){
+        console.log(`pick a second number`)
     }
     console.log(`execution check 3: ${result}`)
-    operatorCheck = false;
-    valueStoreX = result;
-    valueStoreY = 0;
-    operator = ``
-    calculatorDisplay.innerText = String(result)
 
-    console.log(`valueStoreX:`, valueStoreX);
-    console.log(`valueStoreY:`, valueStoreY);
+    operatorCheck = false;
+    a = result;
+    stringA = String(result);
+    b = 0;
+    operator = ``;
+    stringB = ``;
+    if (stringA == Infinity){
+        calculatorDisplay.innerText = `Error`
+        stringA = 0;
+        resultEqualToA = false;
+    } else{
+        calculatorDisplay.innerText = stringA;
+        resultEqualToA = true;
+    }
+    buttonEnter.disabled = true;
+
+    console.log(`stringA:`, stringA);
+    console.log(`stringB:`, stringB);
     console.log(`operator:`, operator);
+    console.log(`result equal to A check 4: ${resultEqualToA}`);
     
     
+}
+
+
+function clearButton(){
+    //disables operator buttons. resets values.
 }
